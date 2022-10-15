c7b0eddf1e74c613906fc3d2ce12d113d82a2cae
Artem Kovalyov <artem.kovalyov@gmail.com>
2022-10-15 23:16:01 +0200

    Add Pill, refactor BlogCard

--- src/lib/components/BlogCard.svelte
+++ src/lib/components/BlogCard.svelte
@@ -1,24 +1,41 @@
 <script lang="ts">
-  export let title = '';
-  export let image = '';
-  export let description = '';
-  export let link = '';
+  import Pill from './Pill.svelte';
+  export let title = 'Very Nice Demo Title';
+  export let image = 'https://placeimg.com/962/558/nature';
+  export let alt = 'Imgage description';
+  export let excerpt =
+    'Some quick example text to build on the card title and make up the bulk of the card content. Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla';
+  export let link = 'https://someoneinmybed.com/';
+  export let tags = ['My Tag 1', 'My Tag 2'];
 </script>
 
-<article class="bg-elevation-1">
-  <a href="">
-    <img class="" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt="" />
+<article class="flex flex-col">
+  <a href={link} class="mb-7 mx-auto">
+    <img class="block bg-elevation-1 max-w-full" src={image} {alt} />
   </a>
-  <header class="">
-    <span class="text-xl  ">TAG</span>
+  <header class="flex flex-col mb-3">
+    <div class="flex justify-between text-sm mb-2">
+      <!-- TAGS -->
+      <div class="flex gap-2">
+        {#if tags.length > 0}
+          {#each tags as tag}
+            <Pill>{tag}</Pill>
+          {/each}
+        {/if}
+      </div>
+      <!-- FEATURED -->
+      <div class="flex gap-2">
+        <span class="text-secondary py-1 px-2">Author: Artem Kovalov</span>
+      </div>
+    </div>
     <a href={link}>
-      <h2 class="text-3xl font-bold ">My Blog content slfkja aslfka, alsjf, allll</h2>
+      <h2 class="text-3xl font-bold ">{title}</h2>
     </a>
   </header>
-  <div class="">
-    Some quick example text to build on the card title and make up the bulk of the card's content.
+  <div class="text-xl mb-2">
+    {excerpt}
   </div>
-  <footer>
+  <footer class="flex justify-between text-sm text-secondary">
     <time datetime="2021-03-18">Mar 18, 2021</time>
     <span>3 min read</span>
   </footer>
---
+++ src/lib/components/Pill.svelte
@@ -0,0 +1,20 @@
+<!-- Renders a Pill. If you pass Link prop it becomes a Pill wih a link. -->
+<script lang="ts">
+  export let link = '';
+</script>
+
+{#if link === ''}
+  <span
+    class="py-1 px-2 bg-surface-variant text-on-surface-variant border border-outline rounded-full"
+  >
+    <slot />
+  </span>
+{:else}
+  <a href={link}>
+    <span
+      class="py-1 px-2 bg-surface-variant text-on-surface-variant border border-outline rounded-full"
+    >
+      <slot />
+    </span>
+  </a>
+{/if}
